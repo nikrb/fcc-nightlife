ea56b440e93aaf68f88e0402effa76bbbcc14fcd
nikrb <nik_rb@yahoo.com>
2017-08-31 00:57:52 +0100

    split search into term and name

--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -5,10 +5,25 @@ import Actions from './Actions';
 export default class HomePage extends React.Component {
   state = {
     businesses: [],
-    highlighted_item: ""
+    highlighted_item: "",
+    // search for something near somewhere
+    term_text: "",
+    location_text: "",
   };
   componentWillMount = () => {
-    Actions.yelpGet({})
+    const term = window.localStorage.getItem( "term_text");
+    const location = window.localStorage.getItem( "location_text");
+    console.log( term, location);
+    if( term || location){
+      this.setState( {term_text:term, location_text: location});
+      this.fetchLocation( {term, location});
+    }
+  };
+  fetchLocation = ( search_query) => {
+    let pl = {};
+    if( search_query.term.length) pl.term = search_query.term;
+    if( search_query.location.length) pl.location = search_query.location;
+    Actions.yelpGet( pl)
     .then( (response) => {
       console.log( "yelp get response:", response);
       this.setState( { businesses: response.data});
@@ -16,6 +31,8 @@ export default class HomePage extends React.Component {
   };
   onBusinessClick = ( name) => {
     console.log( "business clicked:", name);
+    const bus = this.state.businesses.filter( b => b.name === name);
+    window.open( bus[0].url);
   };
   onListItemEnter = (e, name) => {
     this.setState( {highlighted_item: name})
@@ -26,8 +43,18 @@ export default class HomePage extends React.Component {
   onGoingClick = (name) => {
     console.log( "going clicked:", name);
   };
+  onTermTextChange = (e) => {
+    this.setState( {term_text: e.target.value})
+  };
+  onLocationTextChange = (e) => {
+    this.setState( {location_text: e.target.value});
+  };
   onSearchClick = (e) => {
-    console.log( "on going clicked for :", e.target.value);
+    console.log( "search clicked text:", this.state.term_text);
+    const {term_text, location_text} = this.state;
+    window.localStorage.setItem('term_text', term_text);
+    window.localStorage.setItem('location_text', location_text);
+    this.fetchLocation( {term:term_text, location:location_text});
   };
   render = () => {
     const listyle = {
@@ -58,11 +85,17 @@ export default class HomePage extends React.Component {
     const mag = {
       transform: "rotate(-45deg)",
     };
+    const search_style = { margin:"0"};
     return (
       <div className="App">
         <h1>Who's Where?</h1>
         <div>
-          <input />
+          Find&nbsp;
+          <input style={search_style} type="text" onChange={this.onTermTextChange}
+            placeholder="Find pizza, cheap dinner, Max's" value={this.state.term_text} />
+          &nbsp;Near&nbsp;
+          <input style={search_style} type="text" placeholder="Near"
+            onChange={this.onLocationTextChange} value={this.state.location_text}/>
           <button type="button" onClick={this.onSearchClick} >
             <div style={mag}>&#x26B2;</div>
           </button>
@@ -73,4 +106,4 @@ export default class HomePage extends React.Component {
       </div>
     );
   };
-}
+};
