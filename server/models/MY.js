fadc3f54b493f43f53dc682a2e2a738f78b6ed37
nikrb <nik_rb@yahoo.com>
2018-08-09 15:36:17 +0100

    fix: migrate to yelp latest

--- server/models/MY.js
+++ server/models/MY.js
@@ -1,26 +1,18 @@
 require( 'dotenv').config();
 const Yelp = require( 'yelp-fusion');
-let access_token = 0;
-Yelp.accessToken( process.env.yelp_app_id, process.env.yelp_app_secret)
-.then( (response) => {
-  access_token = response.jsonBody.access_token;
-})
-.catch( (err) => {
-  console.error( "my yelp access token failed:", err);
-});
 
 const search = ( {term="food",location="90210", limit=3, offset=0}) => {
-  if( access_token){
+  if( process.env.yelp_app_id){
     console.log( term, location, limit, offset);
-    const client = Yelp.client( access_token);
+    const client = Yelp.client( process.env.yelp_app_id);
     return client.search( { term, location, limit, offset });
   } else {
     console.error( "no access_token");
-    
+
   }
 };
 const reviews = ( id) => {
-  const client = Yelp.client( access_token);
+  const client = Yelp.client( process.env.yelp_app_id);
   return client.reviews( id);
 };
 
